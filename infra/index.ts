bf05d3e5cb3a28451cc0c04ba3259b4212ca9519
ashutoshpith <amxdp18@gmail.com>
2022-09-25 02:37:59 +0530

    oo

--- infra/index.ts
+++ infra/index.ts
@@ -3,9 +3,9 @@ import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
 
 // Create an AWS resource (S3 Bucket)
-// const bucket = new aws.s3.Bucket('my-bucket');
+const bucket = new aws.s3.Bucket('infra-dev-bucket');
 
 // Export the name of the bucket
 // export const bucketName = bucket.id;
 
-export const bucketName = 'Hello';
+export const bucketName = bucket.id;
