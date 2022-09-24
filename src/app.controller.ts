c9a53e3e60fbe6eb366e456df83a96a8054cf736
ashutoshpith <amxdp18@gmail.com>
2022-09-24 23:51:36 +0530

    name chagnes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  home(): string {
+    return 'Version 1.0';
+  }
+
+  @Get('/health')
+  health() {
+    return 'Ok Health';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('Running on 3000');
+
   await app.listen(3000);
 }
 bootstrap();
